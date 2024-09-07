5abe77ededea60568350233160f7ed87cdba34a1
Alexandr K <kurochen@ya.ru>
2024-09-07 18:09:05 +0700

    5.3

---
+++ src/Telega.tsx
@@ -0,0 +1,22 @@
+import React, { useEffect } from "react";
+
+const TelegramComments = () => {
+  useEffect(() => {
+    // Создаем скрипт
+    const script = document.createElement("script");
+    script.async = true;
+    script.src = "https://telegram.org/js/telegram-widget.js?15";
+    script.setAttribute("data-telegram-discussion", "sehouRU/15");
+    script.setAttribute("data-comments-limit", "5");
+    script.setAttribute("data-color", "000000"); // Цветовая схема
+    script.setAttribute("data-dark", "0"); // Темная тема
+    script.setAttribute("data-height", "300px"); // Высота виджета
+
+    // Добавляем скрипт в DOM
+    document.getElementById("telegram-comments").appendChild(script);
+  }, []); // Пустой массив зависимостей, чтобы выполнить только один раз при монтировании компонента
+
+  return <div id="telegram-comments"></div>;
+};
+
+export default TelegramComments;
